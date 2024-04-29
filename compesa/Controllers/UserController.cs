eadfc2bb8dfa2e173164844083a45622caf330fb
Rian <alvesrianpf@gmail.com>
2024-04-28 21:34:19 -0300

    Adicionar arquivos de projeto.

---
+++ compesa/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using compesa.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace compesa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+
+        public UserController(UserContext userContext)
+        {
+            this._userContext = userContext;
+        }
+
+        [HttpGet("/getUsers")]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            var result = await _userContext.Users.Select(
+                s => new User
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Email = s.Email,
+                    Password = s.Password,
+                    Login = s.Login,
+                }
+            ).ToListAsync();
+
+            if (result.Count < 0 ) 
+            { 
+                return NotFound();
+            } else
+            {
+                return result;
+            }
+        }
+
+        [HttpPost("/insertUser")]
+        public async Task<HttpStatusCode> InsertUser(User _user)
+        {
+            var newUser = new User()
+            {
+                Id = _user.Id,
+                Name = _user.Name,
+                Email = _user.Email,
+                Password = _user.Password,
+                Login = _user.Login,
+            };
+
+            _userContext.Users.Add(newUser);
+            await _userContext.SaveChangesAsync();
+
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ compesa/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace compesa.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Login { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
